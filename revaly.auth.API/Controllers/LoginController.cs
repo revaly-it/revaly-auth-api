20c37374c53af8d3f16dde749a2329a1d9803fc1
rafaelx0liveira <rafaelaparecido.oliveirasilva@gmail.com>
2025-04-06 15:29:32 -0300

    feat: mediatr and controllers

---
+++ revaly.auth.API/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace revaly.auth.API.Controllers
+{
+
+    /// <summary>
+    /// Auth Controller
+    /// </summary>
+    [Route("api/auth")]
+    [ApiController]
+    public class LoginController(IMediator mediator) : ControllerBase
+    {
+        /// <summary>
+        /// Login
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        //[HttpPost("login")]
+        //public async Task<IActionResult> Login([FromBody] LoginRequest request)
+        //{
+        //    var result = await mediator.Send(request);
+        //    return Ok(result);
+        //}
+    }
+}
