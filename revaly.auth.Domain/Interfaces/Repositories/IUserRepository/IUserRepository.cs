cc76b4f13f1f0b178583791b90c4f5746e819205
rafaelx0liveira <rafaelaparecido.oliveirasilva@gmail.com>
2025-04-06 16:12:58 -0300

    feat: user repository and interface

---
+++ revaly.auth.Domain/Interfaces/Repositories/IUserRepository/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿
+using revaly.auth.Domain.Entities;
+
+namespace revaly.auth.Domain.Interfaces.Repositories.IUserRepository
+{
+    public interface IUserRepository
+    {
+        Task<User> GetUserByIdAsync(int id);
+        Task<IEnumerable<User>> GetAllUsersAsync();
+        Task AddUserAsync(User user);
+        Task UpdateUserAsync(User user);
+        Task DeleteUserAsync(int id);
+    }
+}
---
+++ revaly.auth.Infrastructure/Repositories/UserRepository/UserRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using revaly.auth.Domain.Entities;
+using revaly.auth.Domain.Interfaces.Repositories.IUserRepository;
+using revaly.auth.Infrastructure.Context;
+
+namespace revaly.auth.Infrastructure.Repositories.UserRepository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly MySQLContext _mySQLContext;
+        public UserRepository(MySQLContext mySQLContext)
+        {
+            _mySQLContext = mySQLContext;
+        }
+        public async Task<User> GetUserByIdAsync(int id)
+        {
+            return await _mySQLContext.Users.FindAsync(id);
+        }
+        public async Task<IEnumerable<User>> GetAllUsersAsync()
+        {
+            return await _mySQLContext.Users.ToListAsync();
+        }
+        public async Task AddUserAsync(User user)
+        {
+            await _mySQLContext.Users.AddAsync(user);
+            await _mySQLContext.SaveChangesAsync();
+        }
+        public async Task UpdateUserAsync(User user)
+        {
+            _mySQLContext.Users.Update(user);
+            await _mySQLContext.SaveChangesAsync();
+        }
+        public async Task DeleteUserAsync(int id)
+        {
+            var user = await GetUserByIdAsync(id);
+            if (user != null)
+            {
+                _mySQLContext.Users.Remove(user);
+                await _mySQLContext.SaveChangesAsync();
+            }
+        }
+    }
+}
