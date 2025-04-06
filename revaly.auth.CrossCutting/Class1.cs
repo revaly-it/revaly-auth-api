f6557dd3d4acbd728715f554c897aaaa3426e79f
rafaelx0liveira <rafaelaparecido.oliveirasilva@gmail.com>
2025-04-05 21:42:19 -0300

    feat: adding vault services in CrossCutting

--- revaly.auth.CrossCutting/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace revaly.auth.CrossCutting
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ revaly.auth.CrossCutting/DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using VaultService.Extensions;
+
+namespace revaly.auth.CrossCutting.DependencyInjection
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddVaultService(configuration);
+            return services;
+        }
+
+        private static IServiceCollection AddVaultService(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var vaultAddress = configuration["KeyVault:Address"] ?? throw new ArgumentNullException("KeyVault Address is missing");
+            var vaultToken = configuration["KeyVault:Token"] ?? throw new ArgumentNullException("KeyVault Token is missing");
+
+            return services.AddVaultService(
+                vaultAddress: vaultAddress,
+                vaultToken: vaultToken
+            );
+        }
+    }
+}
