eea1c870c458bccf1969a8df70a88a217a1e90ec
rafaelx0liveira <rafaelaparecido.oliveirasilva@gmail.com>
2025-04-06 15:29:17 -0300

    feat: add mysql extension

--- revaly.auth.CrossCutting/DependencyInjection/DependencyInjection.cs
+++ revaly.auth.CrossCutting/DependencyInjection/DependencyInjection.cs
@@ -1,22 +1,24 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using revaly.auth.Infrastructure.Context;
 using VaultService.Extensions;
+using VaultService.Interface;
 
 namespace revaly.auth.CrossCutting.DependencyInjection
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructure(
-            this IServiceCollection services,
-            IConfiguration configuration)
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddVaultService(configuration);
+            services
+                .AddVaultService(configuration)
+                .AddMySQLDatabase(configuration);
+
             return services;
         }
 
-        private static IServiceCollection AddVaultService(
-            this IServiceCollection services,
-            IConfiguration configuration)
+        private static IServiceCollection AddVaultService(this IServiceCollection services, IConfiguration configuration)
         {
             var vaultAddress = configuration["KeyVault:Address"] ?? throw new ArgumentNullException("KeyVault Address is missing");
             var vaultToken = configuration["KeyVault:Token"] ?? throw new ArgumentNullException("KeyVault Token is missing");
@@ -26,5 +28,23 @@ namespace revaly.auth.CrossCutting.DependencyInjection
                 vaultToken: vaultToken
             );
         }
+
+        private static IServiceCollection AddMySQLDatabase(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<MySQLContext>((provider, options) =>
+            {
+                var vaultService = provider.GetRequiredService<IVaultClient>();
+
+                var connectionStringKey = configuration["KeyVaultSecrets:Database:ConnectionString"] ?? throw new ArgumentNullException("Connection string is missing");
+
+                var connectionString = vaultService.GetSecret(connectionStringKey);
+
+
+                options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+
+            });
+            return services;
+        }
+        
     }
 }
