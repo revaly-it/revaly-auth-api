ca34427c77c14f8b7e9fbb70bad8aa4a79aaa9da
rafaelx0liveira <rafaelaparecido.oliveirasilva@gmail.com>
2025-04-07 10:21:01 -0300

    feat: result view model

---
+++ revaly.auth.application/Models/ResultViewModel.cs
@@ -0,0 +1,32 @@
+﻿
+namespace revaly.auth.Application.Models
+{
+    public class ResultViewModel
+    {
+        public ResultViewModel(bool isSucess = true, string message = "")
+        {
+            IsSuccess = isSucess;
+            Message = message;
+        }
+
+        public bool IsSuccess { get; private set; }
+        public string Message { get; private set; }
+
+        public static ResultViewModel Success() => new();
+        public static ResultViewModel Error(string message) => new() { IsSuccess = false, Message = message };
+    }
+
+    public class ResultViewModel<T> : ResultViewModel
+    {
+        public ResultViewModel(T? data, bool isSuccess = true, string message = "") : base(isSuccess, message)
+        {
+            Data = data;
+        }
+
+        public T? Data { get; private set; }
+        public static ResultViewModel<T> Success(T data) => new(data);
+        public static ResultViewModel<T> Success(T data, string message) => new(data, true, message);
+        public static ResultViewModel<T> Error(string message) => new(default, false, message);
+    }
+}
+}
