4477c381a7e1f778547cfba1a27b6c0caa31962d
rafaelx0liveira <rafaelaparecido.oliveirasilva@gmail.com>
2025-04-07 17:14:01 -0300

    feat: handlers

---
+++ revaly.auth.application/Handlers/AuthCommandHandler/RegisterUserCommandHandler/RegisterUserCommandHandler.cs
@@ -0,0 +1,55 @@
+﻿
+using MediatR;
+using revaly.auth.Application.Commands.AuthCommand.RegisterUserCommand;
+using revaly.auth.Application.Models;
+using revaly.auth.Domain.Entities;
+using revaly.auth.Domain.Entities.Enums;
+using revaly.auth.Domain.Interfaces.IUnitOfWork;
+
+namespace revaly.auth.Application.Handlers.AuthCommandHandler.RegisterUserCommandHandler
+{
+    public class RegisterUserCommandHandler (IUnitOfWork unitOfWork)
+        : IRequestHandler<RegisterUserCommand, ResultViewModel<UserViewModel>>
+    {
+        private readonly IUnitOfWork _unitOfWork = unitOfWork;
+
+        public async Task<ResultViewModel<UserViewModel>> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var userExists = await _unitOfWork.User.UserExistsAsync(request.Email);
+
+                if (userExists)
+                {
+                    return ResultViewModel<UserViewModel>.Error($"User with email {request.Email} already exists.");
+                }
+
+                var user = new User
+                {
+                    FullName = request.FullName,
+                    Email = request.Email,
+                    PasswordHash = BCrypt.Net.BCrypt.HashPassword(request.Password),
+                    Role = Role.User,
+                    IsActive = true,
+                    CreatedAt = DateTime.UtcNow.AddHours(-3)
+                };
+
+                await _unitOfWork.BeginTransactionAsync();
+                await _unitOfWork.User.AddUserAsync(user);
+                await _unitOfWork.CompleteAsync();
+
+                return ResultViewModel<UserViewModel>.Success(new UserViewModel
+                {
+                    Id = user.Id,
+                    FullName = user.FullName,
+                    Email = user.Email,
+                    CreatedAt = user.CreatedAt
+                });
+            }
+            catch (Exception ex)
+            {
+                return ResultViewModel<UserViewModel>.Error(ex.Message);
+            }
+        }
+    }
+}
--- revaly.auth.application/Handlers/AuthQueryHandler/LoginQueryHandler/LoginQueryHandler.cs
+++ revaly.auth.application/Handlers/AuthQueryHandler/LoginQueryHandler/LoginQueryHandler.cs
@@ -1,22 +1,22 @@
 ﻿using MediatR;
-using revaly.auth.Application.Commands.LoginCommand;
 using revaly.auth.Application.Models;
+using revaly.auth.Application.Queries.LoginQuery;
 using revaly.auth.Application.Services.Interface;
 using revaly.auth.Domain.Interfaces.IUnitOfWork;
 
-namespace revaly.auth.Application.Handlers.LoginCommandHandler
+namespace revaly.auth.Application.Handlers.AuthQueryHandler.LoginQueryHandler
 {
-    public class LoginCommandHandler (
+    public class LoginQueryHandler (
         ITokenService tokenService,
         IUnitOfWork unitOfWork)
-        : IRequestHandler<LoginCommand, ResultViewModel<string>>
+        : IRequestHandler<LoginQuery, ResultViewModel<string>>
     {
         private readonly ITokenService _tokenService = tokenService;
         private readonly IUnitOfWork _unitOfWork = unitOfWork;
 
-        public async Task<ResultViewModel<string>> Handle(LoginCommand request, CancellationToken cancellationToken)
+        public async Task<ResultViewModel<string>> Handle(LoginQuery request, CancellationToken cancellationToken)
         {
-            var user = await _unitOfWork.UserRepository.GetUserByEmailAsync(request.Email);
+            var user = await _unitOfWork.User.GetUserByEmailAsync(request.Email);
 
             if (user == null || !BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash))
             {
