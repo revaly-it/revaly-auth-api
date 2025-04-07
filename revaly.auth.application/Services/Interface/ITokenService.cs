fddc058b377bb2c85553593668bec39a41d9dc94
rafaelx0liveira <rafaelaparecido.oliveirasilva@gmail.com>
2025-04-07 10:21:42 -0300

    feat: registering method in interface

--- revaly.auth.application/Services/Interface/ITokenService.cs
+++ revaly.auth.application/Services/Interface/ITokenService.cs
@@ -1,6 +1,9 @@
-﻿namespace revaly.auth.Application.Services.Interface
+﻿using revaly.auth.Domain.Entities;
+
+namespace revaly.auth.Application.Services.Interface
 {
     public interface ITokenService
     {
+        string GenerateToken(User user);
     }
 }
