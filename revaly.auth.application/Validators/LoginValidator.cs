4b2949a3aa95a4201f563321e554131e80e426f0
rafaelx0liveira <rafaelaparecido.oliveirasilva@gmail.com>
2025-04-07 10:22:06 -0300

    feat: login validator

---
+++ revaly.auth.application/Validators/LoginValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using revaly.auth.Application.Commands.LoginCommand;
+
+namespace revaly.auth.Application.Validators
+{
+    public class LoginValidator : AbstractValidator<LoginCommand>
+    {
+        public LoginValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email is required.")
+                .EmailAddress().WithMessage("Invalid email.");
+
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .WithMessage("Password is required.");
+        }
+    }
+}
