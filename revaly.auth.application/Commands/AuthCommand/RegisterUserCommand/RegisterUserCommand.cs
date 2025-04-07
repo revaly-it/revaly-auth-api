db8fe4f80e794b492aaca159b170ff387ea2bc6c
rafaelx0liveira <rafaelaparecido.oliveirasilva@gmail.com>
2025-04-07 17:13:47 -0300

    refactor: register user command + refactor login query

---
+++ revaly.auth.application/Commands/AuthCommand/RegisterUserCommand/RegisterUserCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using revaly.auth.Application.Models;
+
+namespace revaly.auth.Application.Commands.AuthCommand.RegisterUserCommand
+{
+    public class RegisterUserCommand : IRequest<ResultViewModel<UserViewModel>>
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
